e74c2a5c06e99abd1b897a35e92c89baeb190791
deepa2664 <deepa261@gmail.com>
2025-05-02 13:23:20 +0530

    Update main.tf

--- easy/main.tf
+++ easy/main.tf
@@ -2,19 +2,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# 1. Create source and destination S3 Buckets
+# Source and Destination Buckets
 resource "aws_s3_bucket" "source" {
-  bucket = "my-source-bucket-unique-name789"  # Ensure this name is globally unique
+  bucket = "my-source-bucket-unique-name789"
 }
 
 resource "aws_s3_bucket" "destination" {
-  bucket = "my-destination-bucket-unique-name456"  # Ensure this name is globally unique
+  bucket = "my-destination-bucket-unique-name456"
 }
 
-# 2. IAM role for Lambda to allow access to S3 buckets
+# IAM Role
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda_s3_copy_role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -27,11 +26,10 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
-# 3. IAM policy for Lambda to access the source and destination S3 buckets
+# IAM Policy for Lambda
 resource "aws_iam_role_policy" "lambda_policy" {
   name = "lambda_s3_policy"
   role = aws_iam_role.lambda_exec.id
-
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -55,17 +53,26 @@ resource "aws_iam_role_policy" "lambda_policy" {
   })
 }
 
-# 4. Lambda function that triggers on file upload to S3 and copies the file to the destination bucket
+# Lambda Function
 resource "aws_lambda_function" "s3_copy" {
   function_name    = "S3CopyFunction"
-  filename         = "lambda_function_payload.zip"  # You will need to create and zip the Lambda function code
+  filename         = "lambda_function_payload.zip"
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.9"
   role             = aws_iam_role.lambda_exec.arn
   source_code_hash = filebase64sha256("lambda_function_payload.zip")
 }
 
-# 5. S3 Bucket Notification to trigger Lambda on file upload
+# Lambda Permission
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.s3_copy.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.source.arn
+}
+
+# S3 Notification Trigger
 resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = aws_s3_bucket.source.id
 
@@ -75,13 +82,4 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   }
 
   depends_on = [aws_lambda_permission.allow_s3]
-}
-
-# 6. Allow S3 to invoke Lambda
-resource "aws_lambda_permission" "allow_s3" {
-  statement_id  = "AllowExecutionFromS3"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.s3_copy.function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.source.arn
 }
